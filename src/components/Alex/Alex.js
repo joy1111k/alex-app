b3332d6b1725cb628d96657b6738f47c7ab9a5b2
CL-Gao <94198002+CL-Gao@users.noreply.github.com>
2022-02-23 14:48:02 -0500

    int

---
+++ src/components/Alex/Alex.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Alex() {
+  return <div className='text-red-500'>Alex</div>;
+}
+
+export default Alex;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/components/Alex/Alex.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
